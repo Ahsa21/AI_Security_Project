307a45d9d7e34059b4db4ecf5e5b6e65addf2a26
Ahmad <Ahmadsalouka@gmail.com>
2026-02-21 16:55:16 +0100

    implement flask and load the model to the postrequest

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
 
 app = Flask(__name__)
+
+
+model = joblib.load("original_model.pkl")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.json["features"]
+        data = np.array(data).reshape(1, -1)
+
+        prediction = model.predict(data)[0]
+        probabilities = model.predict_proba(data)[0]
+
+        return jsonify({
+            "prediction": int(prediction),
+            "probabilities": probabilities.tolist()
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
